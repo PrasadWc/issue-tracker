31bb80eff3353d72a7bb12863175be0bc5ebd945
prasad_wick <prasadwc98@gmail.com>
2026-04-22 22:39:55 +0530

    deployment stuff

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,13 @@ connectDB();
 const app = express();
 
 // Middleware
-app.use(cors());
+// Middleware
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL || "*",
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
